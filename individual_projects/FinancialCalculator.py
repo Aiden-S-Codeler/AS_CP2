7015fec20ecee5903776db946551b49fb6bac266
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2026-01-11 02:50:15 +0000

    Finished Financing

--- individual_projects/FinancialCalculator.py
+++ individual_projects/FinancialCalculator.py
@@ -1,9 +1,5 @@
 #Financial Calculator AS 2nd
-
-#Make function to get percentage of a number
-def percent(num, perc):
-    final_number = (num/100)*perc
-    return final_number
+import time
 
 #Make function to check if input is a number
 def is_num(user_input):
@@ -16,7 +12,7 @@ def is_num(user_input):
 def savings():
     #ask how often user deposits
     while True:
-        frequency = input("How long are you saving? 1:Weekly  2:Monthly\n")
+        frequency = input("How often do you deposit? 1:Weekly  2:Monthly\n")
         if frequency == "1" or frequency == "2":
             break
         else:
@@ -38,7 +34,7 @@ def savings():
 
     #ask user for savings goal
     while True:
-        goal = input("How much do you goal at once?\n")
+        goal = input("How much is your goal?\n")
         if is_num(goal) == True:
             goal = int(goal)
             break
@@ -51,8 +47,10 @@ def savings():
     
     if frequency == "1":
         print(f"It will take you {goal/deposit} weeks to save up ${goal}")
+        time.sleep(1)
     else:
         print(f"It will take you {goal/deposit} months to save up ${goal}")
+        time.sleep(1)
 
 #Make function for compound interest
 def compound():
@@ -84,13 +82,166 @@ def compound():
     
     #ask user for how long they wait
     while True:
-        time = input("How long will you leave your money?\n")
-        if is_num(time) == True:
-            time = int(time)
+        itime = input("How many years will you let your money collect interest?\n")
+        if is_num(itime) == True:
+            itime = int(itime)
             break
-        elif is_num(time) == False:
+        elif is_num(itime) == False:
             print("Invalid choice.")
             continue
         else:
             print("Something broke.")
             continue
+    
+    #make inner function to do compund interest
+    def biggering(start, interest, itime):
+        for i in range(itime):
+            start += (start/100)*interest
+            start = round(start,)
+        return start
+    
+    print(biggering(start, interest, itime))
+    time.sleep(1)
+
+#make function for budgeting
+def budget():
+
+    #ask for amount of categories
+    while True:
+        category_count = input("How many budget categories do you have?\n")
+        if is_num(category_count) == True:
+            category_count = int(category_count)
+            break
+        elif is_num(category_count) == False:
+            print("Invalid choice.")
+            continue
+        else:
+            print("Something broke.")
+            continue
+    
+    #make list to hold categories later
+    categories = []
+    
+    #get new categories
+    for i in range(category_count):
+        category_add = input(f"What is budget category {i+1}?\n")
+        categories.append(category_add)
+
+    #get monthly income
+    while True:
+        income = input("What is your monthly income?\n")
+        if is_num(income) == True:
+            income = int(income)
+            break
+        elif is_num(income) == False:
+            print("Invalid choice.")
+            continue
+        else:
+            print("Something broke.")
+            continue
+    
+    #make budget plan
+    for c in categories:
+        while True:
+            spending = input(f"What percentage of your income goes to {c}?\n")
+            if is_num(spending) == True:
+                spending = int(spending)
+                break
+            elif is_num(spending) == False:
+                print("Invalid choice.")
+                continue
+            else:
+                print("Something broke.")
+                continue
+        print(f"{c} is {round((income/100)*spending, 2)}")
+        time.sleep(1)
+
+#sales calculator
+def sales_price():
+    #ask user for starting cost
+    while True:
+        start = input("What is your starting price?\n")
+        if is_num(start) == True:
+            start = int(start)
+            break
+        elif is_num(start) == False:
+            print("Invalid choice.")
+            continue
+        else:
+            print("Something broke.")
+            continue
+    
+    #ask user for discount
+    while True:
+        discount = input("What is the discount?\n")
+        if is_num(discount) == True:
+            discount = int(discount)
+            break
+        elif is_num(discount) == False:
+            print("Invalid choice.")
+            continue
+        else:
+            print("Something broke.")
+            continue
+    
+    #give final price
+    print(f"The final price is ${start - ((start/100)*discount)}")
+    time.sleep(1)
+
+#tip calculator
+def tip():
+    #ask user for starting cost
+    while True:
+        start = input("What is your starting price?\n")
+        if is_num(start) == True:
+            start = int(start)
+            break
+        elif is_num(start) == False:
+            print("Invalid choice.")
+            continue
+        else:
+            print("Something broke.")
+            continue
+    
+    #ask user for discount
+    while True:
+        tip = input("How much will you tip?\n")
+        if is_num(tip) == True:
+            tip = int(tip)
+            break
+        elif is_num(tip) == False:
+            print("Invalid choice.")
+            continue
+        else:
+            print("Something broke.")
+            continue
+    
+    #give final price
+    print(f"The tip is {(start/100)*tip} and the final price is ${start + ((start/100)*tip)}")
+    time.sleep(1)
+
+print("Welcome to the financial calulator")
+while True:
+    uchoice = input("Would you like to:\n1: Calculate how long it will take to save for a goal\n2: Calculate compound interest\n3: Make a budget\n4: Calculate the pre-tax sales price\n5: Calculate a tip\n6: leave\nplease enter the numeric value for your choice: ")
+    if uchoice == "1":
+        savings()
+        pass
+    elif uchoice == "2":
+        compound()
+        pass
+    elif uchoice == "3":
+        budget()
+        pass
+    elif uchoice == "4":
+        sales_price()
+        pass
+    elif uchoice == "5":
+        tip()
+        pass
+    elif uchoice == "6":
+        print("See you soon!")
+        time.sleep(1.0)
+        break
+    else:
+        print("Invalid choice")
+        continue
