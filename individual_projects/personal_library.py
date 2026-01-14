5400a8f3a33782d935a3dd9b30ffe09f5f6f6ae2
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2026-01-14 10:50:34 -0700

    made a view function

---
+++ individual_projects/personal_library.py
@@ -0,0 +1,14 @@
+#AS 2nd personal library
+
+#make a function to view all things in libraries
+def view(library, exist):
+    if exist == True:
+        print("Your library includes:")
+        for i in library:
+            print(f"{library[i]} by {i}")
+    else:
+        print("You do not yet have a library")
+
+#make function to add things to library
+def add(library, exist):
+    Author = input("")
