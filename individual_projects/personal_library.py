802218c9199229959745c885eec7b32b4ebe64f6
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2026-01-21 00:42:17 +0000

    Finished library

--- individual_projects/personal_library.py
+++ individual_projects/personal_library.py
@@ -12,7 +12,7 @@ def view(library, exist):
 #make function to add things to library
 def add(library, exist):
     Author = input("Who wrote the book? ")
-    if exist == True:
+    if library:
         library[Author] = {input("What is the book title? ")}
         print(f"Added {library[Author]} by {Author} to library. If you had another book by that author, it was overwritten.")
     else:
@@ -23,7 +23,7 @@ def add(library, exist):
 
 #make function for removing book
 def remove(library, exist):
-    if exist == True:
+    if library:
         print("Your library includes:")
         for i in library:
             print(f"{library[i]} by {i}")
@@ -38,7 +38,44 @@ def remove(library, exist):
                 print(f"There is no author under the name {Author}")
                 continue
     else:
+        print("You do not yet have a library")
+
+#make function for searching
+def search(library, exist):
+    if library:
+        while True:
+            Author = input("What author would you like to search? ")
+            if Author in library:
+                print(f"Found: {library[Author]} by {Author}.")
+                break
+            else:
+                print(f"There is no author under the name {Author}")
+                continue
+    else:
         print("You do not yet have a library.")
 
-#make function for
-add({"Larry": "It"}, True)
+#make function for running code
+def run(library, exist):
+    while True:
+        choice = input("Would you like to 1: search for an author, 2: add a book, 3: remove an author, 4: view your library, or 5: leave? Please enter the numeric value of your choice. ")
+        if choice == "1":
+            search(library, exist)
+            continue
+        elif choice == "2":
+            library, exist = add(library, exist)
+            continue
+        elif choice == "3":
+            library = remove(library, exist)
+            continue
+        elif choice == "4":
+            view(library, exist)
+            continue
+        elif choice == "5":
+            break
+        else:
+            print("Invalid choice.")
+            continue
+
+print("Welcome to your library.")
+run({}, False)
+print("See you later!")
