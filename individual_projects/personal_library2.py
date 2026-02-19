e2e43569106d68f8f2856cb7e0318cf17126182f
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2026-02-19 00:31:38 +0000

    finished project

--- individual_projects/personal_library2.py
+++ individual_projects/personal_library2.py
@@ -9,15 +9,14 @@ def view():
             if line[0] == 'Title':
                 pass
             else:
-                print(f"{line[0]}, written by {line[1]}, genre: {line[2]}")
+                print(f"{line[0]}, written by {line[1]}, genre: {line[2]}, Pages: {line[3]}")
 
 #make function to add things to library
 def add():
     with open("individual_projects/personal_library2.csv", mode= 'a', newline= '') as sample:
-        reader = csv.reader(sample)
-        fieldnames = ['Title','Author','Genre']
+        fieldnames = ['Title','Author','Genre','Length']
         writer = csv.DictWriter(sample, fieldnames=fieldnames)
-        stuff = {'Title':input("What is the title? ").lower().title().strip(), 'Author':input("Who is the author? ").lower().title().strip(), 'Genre':input("What is the genre? ").lower().title().strip(), 'Length':input("What is the length? ").lower().title().strip()}
+        stuff = {'Title':input("What is the title? ").lower().title().strip(), 'Author':input("Who is the author? ").lower().title().strip(), 'Genre':input("What is the genre? ").lower().title().strip(), 'Length':input("What is the length? ").strip()}
         writer.writerow(stuff)
         
         
@@ -32,7 +31,7 @@ def remove():
             if line[0] == 'Title':
                 pass
             else:
-                all.append({'Title':line[0] ,'Author':line[1] ,'Genre':line[2]})
+                all.append({'Title':line[0] ,'Author':line[1] ,'Genre':line[2], 'Length':line[3]})
         for line in all:
             if line["Title"] == uinput:
                 all.remove(line)
@@ -51,12 +50,12 @@ def search():
         uinput = input("Input title, author, or genre you would like to search for: ")
         for line in reader:
             if uinput.lower().title().strip() in line:
-                print(f"{line[0]}, written by {line[1]}, genre: {line[2]}")
+                print(f"{line[0]}, written by {line[1]}, genre: {line[2]}, length: {line[3]}")
             else:
                 pass
 
 #make function for running code
-def run(library, exist):
+def run():
     while True:
         choice = input("Would you like to 1: search for a book, 2: add a book, 3: remove a book, 4: view your library, or 5: leave? Please enter the numeric value of your choice. ")
         if choice == "1":
@@ -78,5 +77,5 @@ def run(library, exist):
             continue
 
 print("Welcome to your library.")
-run({}, False)
+run()
 print("See you later!")
