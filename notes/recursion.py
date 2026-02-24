4adad399080affaaf1c694d5c6613de0a3ea620e
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2026-02-24 10:45:18 -0700

    jzkhbvc

---
+++ notes/recursion.py
@@ -0,0 +1,39 @@
+number = 5
+factorial = 1
+
+while number > 0:
+    factorial *= number
+    number -= 1
+
+print(factorial)
+
+def factor(num):
+    if num == 1: return 1
+    return num * factor(num-1)
+
+print(factor(5))
+
+number = 10
+sequence = [1, 1]
+for i in range(1, number):
+    sequence.append(sequence[i] + sequence[i-1])
+print(sequence)
+
+sequence_2 = [1, 1]
+'''def fibonacci(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        sequence_2.append(sequence_2[fibonacci(n)] + sequence_2[fibonacci(n-1)])'''
+
+def fibonacci(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(10))
